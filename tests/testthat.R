f4fdb62e671eb7cfb60c4436ac41e6e61cae2bcb
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-03-09 23:34:07 +0900

    :white_check_mark: Add minimum tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(tekkamaki)
+
+test_check("tekkamaki")
---
+++ tests/testthat/test-run.R
@@ -0,0 +1,8 @@
+context("test-run")
+
+test_that("tekka runs", {
+  expect_silent(result <- tekka())
+  expect_s3_class(result, "data.frame")
+  expect_s3_class(result$sample_family[[1L]], "data.frame")
+  expect_s3_class(result$demography[[1L]], "data.frame")
+})
