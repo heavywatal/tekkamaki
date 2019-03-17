28a595d74e57533dc77e13c358f9932d7ca49675
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-03-18 00:52:54 +0900

    :white_check_mark: Add tests for getter functions

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -3,3 +3,10 @@ context("test-misc")
 test_that("nbinom test runs", {
   expect_s3_class(plot_nbinom(), c("gg", "ggplot"))
 })
+
+test_that("getter functions work", {
+  expect_equal(class(migration_matrices()[[1]]), "matrix")
+  expect_type(natural_mortality(), "double")
+  expect_type(fishing_mortality(), "double")
+  expect_type(weight_for_age(), "double")
+})
