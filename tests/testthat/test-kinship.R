fed6feb935131ebc1bd9b6c7c12414d14b1551b6
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-03-17 22:34:45 +0900

    :white_check_mark: Add tests for write_hsp/write_pop

--- tests/testthat/test-kinship.R
+++ tests/testthat/test-kinship.R
@@ -10,6 +10,11 @@ test_that("Graph functions works", {
 test_that("POP and HSP works", {
   result = tekka()
   samples = result$sample_family[[1L]]
-  expect_s3_class(as_hsp(samples), "data.frame")
-  expect_s3_class(as_pop(samples), "data.frame")
+  expect_s3_class({hsp = as_hsp(samples)}, "data.frame")
+  expect_s3_class({pop = as_pop(samples)}, "data.frame")
+  path = tempfile()
+  expect_silent(write_hsp(hsp, path))
+  expect_error(write_hsp(pop, path))
+  expect_silent(write_pop(pop, path))
+  expect_error(write_pop(hsp, path))
 })
