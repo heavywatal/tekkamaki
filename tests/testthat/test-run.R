7e1c99d58e860191938bb6cda6d49e50c5cd9e3b
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-03-17 23:16:32 +0900

    :white_check_mark: Silence EXIT_SUCCESS from "tekka -h"

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -1,9 +1,16 @@
 context("test-run")
 
+redirect = function(expr, type = c("message", "output"), file = "/dev/null") {
+  type = match.arg(type)
+  on.exit(sink(NULL, type = type))
+  sink(file, type = type)
+  invisible(eval(expr))
+}
+
 test_that("tekka runs", {
   expect_silent({result = tekka()})
   expect_s3_class(result, "data.frame")
   expect_s3_class(result$sample_family[[1L]], "data.frame")
   expect_s3_class(result$demography[[1L]], "data.frame")
-  expect_output(tekka("-h"), "Usage: tekka")
+  expect_output(redirect(tekka("-h")), "Usage: tekka")
 })
