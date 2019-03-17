97662280887242a075780a91b2dbb00fb7424d3f
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-03-18 00:29:30 +0900

    :white_check_mark: Use capture.output(..., type = "message") for silencing

--- tests/testthat/test-run.R
+++ tests/testthat/test-run.R
@@ -1,16 +1,9 @@
 context("test-run")
 
-redirect = function(expr, type = c("message", "output"), file = "/dev/null") {
-  type = match.arg(type)
-  on.exit(sink(NULL, type = type))
-  sink(file, type = type)
-  invisible(eval(expr))
-}
-
 test_that("tekka runs", {
   expect_silent({result = tekka()})
   expect_s3_class(result, "data.frame")
   expect_s3_class(result$sample_family[[1L]], "data.frame")
   expect_s3_class(result$demography[[1L]], "data.frame")
-  expect_output(redirect(tekka("-h")), "Usage: tekka")
+  expect_output(capture.output(tekka("-h"), type = "message"), "Usage: tekka")
 })
