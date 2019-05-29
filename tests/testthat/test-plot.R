5de41fd8c37eba28a1d423db9d20004fbb862c0e
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-05-29 21:12:33 +0900

    :white_check_mark: Add missing tests for augment()

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,11 +1,15 @@
 test_that("augment/plot methods work", {
   result = tekka("--seed 42 -n80 -y40 -K90 -r1 -l2 --sa 2,2 --sj 2,2")
   samples = result$sample_family[[1L]]
+
   expect_silent({
     df = augment(samples)
   })
   .colnames = c("from", "to", "x", "y", "xend", "yend", "label", "sampled")
   expect_true(all(.colnames %in% names(df)))
+  expect_equal(augment(samples, layout_demography), df)
+  expect_error(augment(samples, 666), "Invalid type")
+
   expect_silent({
     p = plot(samples)
   })
