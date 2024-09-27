9ac6ff6f7e7882387b160ce27b0fe808fecb0648
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-09-27 12:44:40 +0900

    :art: Separate genealogy.R from snp.R

---
+++ R/genealogy.R
@@ -0,0 +1,68 @@
+#' Functions to generate gene genealogy from sample family table
+#'
+#' [gather_segments()] transforms an individual-based `sample_family` into
+#' a segment-based table.
+#' @param samples A `sample_family` data.frame.
+#' @rdname genealogy
+#' @export
+gather_segments = function(samples) {
+  samples |>
+    dplyr::select(!"location") |>
+    tidyr::pivot_longer(dplyr::ends_with("_id"), names_to = "homolog", values_to = "parent_id") |>
+    dplyr::mutate(homolog = c(mother_id = 1L, father_id = 2L)[.data$homolog]) |>
+    dplyr::arrange(-.data$birth_year, .data$id, .data$homolog) |>
+    dplyr::mutate(id = paste(.data$id, .data$homolog, sep = "-"), homolog = NULL)
+}
+
+#' @details
+#' [make_gene_genealogy()] generates a random gene genealogy from a
+#' segment-based table.
+#' @param segments An output from [gather_segments()].
+#' @rdname genealogy
+#' @export
+make_gene_genealogy = function(segments) {
+  x = sample.int(2L, nrow(segments), replace = TRUE)
+  df = segments |>
+    dplyr::mutate(from = paste(.data$parent_id, x, sep = "-")) |>
+    dplyr::mutate(from = stringr::str_replace(.data$from, "^0-[12]$", "0")) |>
+    dplyr::select("from", to = "id", "birth_year", "capture_year")
+  graph = igraphlite::graph_from_data_frame(df)
+  v0 = igraphlite::as_vids(graph, "0")
+  igraphlite::delete_vertices(graph, v0)
+  class(graph) = c("genealogy", class(graph))
+  graph
+}
+
+#' @details
+#' [count_uncoalesced()] counts uncoalesced roots of the samples.
+#' @param genealogy An output from [make_gene_genealogy()].
+#' @rdname genealogy
+#' @export
+count_uncoalesced = function(genealogy) {
+  v_src = igraphlite::Vsource(genealogy)
+  is_src = igraphlite::igraph_from(genealogy) %in% v_src
+  annotate_sampled(genealogy)
+  in_genealogy = !is.na(igraphlite::Eattr(genealogy)$sampled)
+  sum(is_src & in_genealogy)
+}
+
+#' @details
+#' [annotate_sampled()] adds "sampled" edge attribute: `TRUE` if sampled,
+#' `FALSE` if upstream of samples, `NA` if unrelated.
+#' @rdname snp
+#' @export
+annotate_sampled = function(genealogy) {
+  if (!"sampled" %in% names(igraphlite::Eattr(genealogy))) {
+    binary = edge_sampled(genealogy)
+    v_to = igraphlite::igraph_to(genealogy)
+    v_up = igraphlite::upstream_vertices(genealogy, v_to[binary])
+    igraphlite::Eattr(genealogy)$sampled = ifelse(v_to %in% v_up, binary, NA)
+  }
+  genealogy
+}
+
+edge_sampled = function(genealogy) {
+  !is.na(igraphlite::Eattr(genealogy)$capture_year)
+}
+
+areTRUE = function(x) !is.na(x) & x
--- R/snp.R
+++ R/snp.R
@@ -1,16 +1,16 @@
-#' Functions to generate gene genealogy and SNP matrix
+#' Functions to generate SNP matrix
 #'
 #' @details
 #' [make_snp()] is a shortcut to generate a SNP matrix from
 #' a `sample_family` data.frame.
-#' @param .tbl `sample_family` data.frame.
+#' @inheritParams make_gene_genealogy
 #' @param ss A sequence of `segsites`;
 #'   its length is the number of segments;
 #'   each element is the number of segsites on a segment.
 #' @rdname snp
 #' @export
-make_snp = function(.tbl, ss = c(2L, 2L)) {
-  segments = gather_segments(.tbl)
+make_snp = function(samples, ss = c(2L, 2L)) {
+  segments = gather_segments(samples)
   matrices = lapply(ss, \(segsites) {
     make_gene_genealogy(segments) |> make_snp_chromosome(segsites)
   })
@@ -36,39 +36,6 @@ make_snp_chromosome = function(genealogy, segsites) {
 }
 
 #' @details
-#' [gather_segments()] transforms an individual-based `sample_family` into
-#' a segment-based table.
-#' @rdname snp
-#' @export
-gather_segments = function(.tbl) {
-  .tbl |>
-    dplyr::select(!"location") |>
-    tidyr::pivot_longer(dplyr::ends_with("_id"), names_to = "homolog", values_to = "parent_id") |>
-    dplyr::mutate(homolog = c(mother_id = 1L, father_id = 2L)[.data$homolog]) |>
-    dplyr::arrange(-.data$birth_year, .data$id, .data$homolog) |>
-    dplyr::mutate(id = paste(.data$id, .data$homolog, sep = "-"), homolog = NULL)
-}
-
-#' @details
-#' [make_gene_genealogy()] generates a random gene genealogy from a
-#' segment-based table.
-#' @param segments An output from [gather_segments()].
-#' @rdname snp
-#' @export
-make_gene_genealogy = function(segments) {
-  x = sample.int(2L, nrow(segments), replace = TRUE)
-  df = segments |>
-    dplyr::mutate(from = paste(.data$parent_id, x, sep = "-")) |>
-    dplyr::mutate(from = stringr::str_replace(.data$from, "^0-[12]$", "0")) |>
-    dplyr::select("from", to = "id", "birth_year", "capture_year")
-  graph = igraphlite::graph_from_data_frame(df)
-  v0 = igraphlite::as_vids(graph, "0")
-  igraphlite::delete_vertices(graph, v0)
-  class(graph) = c("genealogy", class(graph))
-  graph
-}
-
-#' @details
 #' [place_mutations()] generates a SNP matrix by randomly placing a fixed
 #' number of mutations on a given genealogy.
 #' @param segsites The number of segregating sites on a segment.
@@ -91,38 +58,6 @@ place_mutations = function(genealogy, segsites, v_sampled = NULL) {
   mode(res) = "integer"
   rownames(res) = igraphlite::Vnames(genealogy)[v_sampled]
   res
-}
-
-#' @details
-#' [count_uncoalesced()] counts uncoalesced roots of the samples.
-#' @param genealogy An output from [make_gene_genealogy()].
-#' @rdname snp
-#' @export
-count_uncoalesced = function(genealogy) {
-  v_src = igraphlite::Vsource(genealogy)
-  is_src = igraphlite::igraph_from(genealogy) %in% v_src
-  annotate_sampled(genealogy)
-  in_genealogy = !is.na(igraphlite::Eattr(genealogy)$sampled)
-  sum(is_src & in_genealogy)
-}
-
-#' @details
-#' [annotate_sampled()] adds "sampled" edge attribute: `TRUE` if sampled,
-#' `FALSE` if upstream of samples, `NA` if unrelated.
-#' @rdname snp
-#' @export
-annotate_sampled = function(genealogy) {
-  if (!"sampled" %in% names(igraphlite::Eattr(genealogy))) {
-    binary = edge_sampled(genealogy)
-    v_to = igraphlite::igraph_to(genealogy)
-    v_up = igraphlite::upstream_vertices(genealogy, v_to[binary])
-    igraphlite::Eattr(genealogy)$sampled = ifelse(v_to %in% v_up, binary, NA)
-  }
-  genealogy
-}
-
-edge_sampled = function(genealogy) {
-  !is.na(igraphlite::Eattr(genealogy)$capture_year)
 }
 
 prepare_snp = function(genealogy, segsites) {
@@ -183,5 +118,3 @@ switch_homolog = function(name) {
 rmultinom1 = function(n, k) {
   stats::rmultinom(1L, n, rep_len(1, k)) |> as.vector()
 }
-
-areTRUE = function(x) !is.na(x) & x
