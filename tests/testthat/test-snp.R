a4a169f2b417a17c363743195d11df79c50b1626
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-09-30 13:17:42 +0900

    :white_check_mark: Improve test-snp.R

--- tests/testthat/test-snp.R
+++ tests/testthat/test-snp.R
@@ -1,26 +1,25 @@
 test_that("genealogy methods and make_snp() work", {
   result = tekka("--seed 42 -y40 -K100 -r2 -l2 --sa 2,2 --sj 2,2")
   samples = result$sample_family[[1L]]
-  expect_silent({
-    segments = gather_segments(samples)
-  })
-  expect_silent({
-    genealogy = make_gene_genealogy(segments)
-  })
+  segments = gather_segments(samples) |>
+    expect_silent()
+  genealogy = make_gene_genealogy(segments) |>
+    expect_silent()
   expect_type(count_uncoalesced(genealogy), "integer")
-  expect_silent({
-    df = augment(genealogy)
-  })
+  df = augment(genealogy) |>
+    expect_silent()
   .colnames = c("from", "to", "x", "y", "xend", "yend", "label", "sampled")
   expect_true(all(.colnames %in% names(df)))
-  expect_silent({
-    p = plot(genealogy)
-  })
-  expect_s3_class(p, c("gg", "ggplot"))
+  p = plot(genealogy) |>
+    expect_silent() |>
+    expect_s3_class(c("gg", "ggplot"))
 
   nsam = sum(!is.na(samples$capture_year))
   ss = c(2L, 3L)
-  expect_silent({
-    m = make_snp(samples, ss = ss)
-  })
+  m = make_snp_chromosome(genealogy, ss[1L]) |>
+    expect_silent()
+  expect_identical(dim(m), c(2L * nsam, ss[1L]))
+  l = make_snp(samples, ss = ss) |>
+    expect_silent() |>
+    expect_length(length(ss))
 })
